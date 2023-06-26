db371b3746b5312be12400d783f2aaae868e3581
Your Name <you@example.com>
2023-06-26 12:05:53 +0200

    CARRUSEL

--- src/components/Carrusel/carrusel.jsx
+++ src/components/Carrusel/carrusel.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import styles from "./carrusel.module.css";
+
 import ImagePc from "../../assets/images/pc.png";
-import TitelCarrusel from "../titel-funcion/titel-funcion.jsx";
-import TextCarrusel from "../../components/text-funcion/text-funcion.jsx";
+import TitelCarrusel from "../Titel-funcion/titel-funcion.jsx";
+import SubtextCarrusel from "../Subtext-funcion/subtext-funcion.jsx";
 import ImageFunction from "../Image-funcion/image-funcion.jsx";
 
 export default function Carrusel() {
@@ -10,7 +10,7 @@ export default function Carrusel() {
     <div className="carrusel">
 
         <TitelCarrusel/>
-        <TextCarrusel/>
+        <SubtextCarrusel/>
         <ImageFunction/>
 
 
