7dc71918fc1f1ea5c0e164894edc27189c9dc066
juanan-93 <juanan-93@hotmail.com>
2023-07-02 21:39:03 +0200

    intento from

--- src/components/Footer-from/from.jsx
+++ src/components/Footer-from/from.jsx
@@ -4,20 +4,25 @@ import styles from "./from.module.css";
 
 
 
+
+
+export default function From() {
+
+
   const[value, setValue]=useState("");
   const maxLength=100;
 
   const limit = (e) => {
     const text = e.target.value;
+    
     if (text.length <= maxLength) {
       setValue(text);
     }
-  }
+  };
 
 
 
 
-export default function From() {
   return (
 
     
@@ -32,15 +37,15 @@ export default function From() {
         
         <div className={styles.name}>
           <label for="nombre">Nombre:</label>
-          <input type="text" id="nombre" name="nombre" placeholder="Ingresa tu nombre" />
+          <input type="text" id="nombre" name="nombre" placeholder="Ingresa tu nombre"/>
         </div>
         <div>
           <label for="email">Email:</label>
-          <input type="email" id="email" name="email" placeholder="Ingresa tu email" />
+          <input type="email" id="email" name="email"  placeholder="Ingresa tu email" />
         </div>
         <div>
           <label for="mensaje">Mensaje:</label>
-          <textarea id="mensaje" name="mensaje" placeholder="Escribe tu mensaje" value={value} maxLength={maxLength} onChange={limit}></textarea>
+          <textarea id="mensaje" name="mensaje" placeholder="Escribe tu mensaje" value={value} onChange={limit} maxLength={maxLength}></textarea>
         </div>
         <div className={styles.fromButton}>
           <button className={styles.buttonFrom} type="submit">Enviar</button>
@@ -51,4 +56,5 @@ export default function From() {
     </div>
 
   );
+
 }
